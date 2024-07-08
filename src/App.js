8678c5f8dc68427c265d3957f19a98e91ab3117e
Nikola Simunoski <47476124+NikolaSimunoski@users.noreply.github.com>
2024-07-08 18:34:20 +0200

    - added butons for Step - buttons for count and finished logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DateCounter />
     </div>
   );
 }
 
-export default App;
+function DateCounter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  // classic way of function for set value to state
+  // function increaseStep() {
+  //   //setStep(step + 1);
+  //   setStep((i) => i + 1);
+  // }
+
+  // function decreaseStep() {
+  //   setStep((i) => i - 1);
+  // }
+
+  // function countIncrease() {
+  //   setCount(count + 1);
+  // }
+
+  // function countDecrease() {
+  //   setCount(count - 1);
+  // }
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <p style={{ fontWeight: "bold", margin: "5px" }}>Step: {step}</p>
+        <button onClick={() => setStep((i) => i + 1)}>+</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <p style={{ fontWeight: "bold", margin: "5px" }}>Count: {count}</p>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p style={{ textAlign: "center", fontSize: "24px" }}>
+        <span>
+          {/* ternary operator */}
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} day ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
